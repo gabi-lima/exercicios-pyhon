b45fcb555fe1765aaae0cb09bcd137fea5e89634
gabi-lima <gabriel1277lima@gmail.com>
2022-12-08 03:30:47 -0300

    exercicio 1-4

---
+++ exercicio-1-4.py
@@ -0,0 +1,15 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+print('Vamos descobrir se o número é positivo ou negativo!')
+print('Digite o número escolhido:')
+numero = int(input())
+medidor = 0
+
+
+if numero == medidor:
+    print('o número é igual a zero.')
+else:
+    if numero > medidor:
+        print ('o número é positivo, parabéns!')
+    else: 
+        print ('o número é negativo, sinto muito...')
